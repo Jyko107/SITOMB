87052030432d69f80c284c5da35d7e163b222203
Jyko107 <joykoozon@gmail.com>
2024-05-30 20:33:28 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -36,21 +36,7 @@ else:
         st.experimental_rerun()
 
         st.experimental_rerun()
-    #animate
-    def load_lottieurl(url):
-        r = requests.get(url)
-        if r.status_code != 200:
-            return None
-        return r.json()
-
-    #css
-    def local_css(file_name):
-        with open(file_name) as f:
-            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-
-    #ambil file css
-    local_css("style/style.css")
+  
 
     #header
     with st.container():
